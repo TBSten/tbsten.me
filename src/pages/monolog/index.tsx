2dc28e358874d92209ce5266eb06ea79fe82501c
TBSten <81161390+TBSten@users.noreply.github.com>
2023-05-11 22:23:57 +0900

    monologのlinkコピー機能の追加など

--- src/pages/monolog/index.tsx
+++ src/pages/monolog/index.tsx
@@ -1,3 +1,4 @@
+import { useCopy } from '@/client/copy';
 import Loading from '@/components/Loading';
 import MarkdownCacheProvider from '@/components/MarkdownCacheProvider';
 import MarkdownText from '@/components/MarkdownText';
@@ -10,7 +11,8 @@ import { getMonologList } from '@/monolog/server';
 import { Monolog } from '@/monolog/type';
 import dayjs from 'dayjs';
 import { GetServerSideProps, NextPage } from 'next';
-import { FC } from 'react';
+import { FC, useEffect, useState } from 'react';
+import { BiCopy } from 'react-icons/bi';
 import markdownToHtml from 'zenn-markdown-html';
 
 interface Props {
@@ -42,6 +44,7 @@ const MonologListPage: NextPage<Props> = ({ monologList: defaultMonologList, mar
                             />
                         )}
                     </LayoutContent>
+                    <div className="h-[50vh]" />
                 </MarkdownCacheProvider>
             </BasicLayout>
         </>
@@ -83,14 +86,54 @@ interface MonologCardProps {
     monolog: Monolog
 }
 const MonologCard: FC<MonologCardProps> = ({ monolog }) => {
+    const { copy, isCoping, isSuccess: _isSuccess, isError } = useCopy()
+    const [isSuccess, setIsSuccess] = useState(_isSuccess)
+    useEffect(() => {
+        let clear = setTimeout(() => {
+            setIsSuccess(false)
+        }, 5000)
+        if (_isSuccess)
+            setIsSuccess(_isSuccess)
+        return () => {
+            clearTimeout(clear)
+        }
+    }, [_isSuccess])
     return (
         <div className='p-2 border border-secondary my-2' id={monolog.slug}>
             <MarkdownText
                 markdown={monolog.content}
             />
-            <div className="p-2 text-end text-sm">
-                {dayjs(monolog.publishAt).format("MM月DD日 hh:mm")}
+            <div className="p-2 flex justify-between items-center text-sm">
+                <div>
+                    <button className="btn btn-ghost btn-circle btn-sm md:btn-md" onClick={() => copy(`https://tbsten.me/monolog#${monolog.slug}`)}>
+                        <BiCopy />
+                    </button>
+                </div>
+                <span>
+                    {dayjs(monolog.publishAt).format("MM月DD日 hh:mm")}
+                </span>
             </div>
+            {isCoping &&
+                <div className="toast toast-start toast-bottom">
+                    <div className="alert shadow alert-info">
+                        <Loading />
+                    </div>
+                </div>
+            }
+            {isSuccess &&
+                <div className="toast toast-start toast-bottom">
+                    <div className="alert shadow alert-success">
+                        URLをコピーしました!
+                    </div>
+                </div>
+            }
+            {isError &&
+                <div className="toast toast-start toast-bottom">
+                    <div className="alert shadow alert-error">
+                        コピーできませんでした...
+                    </div>
+                </div>
+            }
         </div>
     );
 }
