60c9a42b4e94b83d2a83da2ee291714b9262fbe0
TBSten <81161390+TBSten@users.noreply.github.com>
2023-05-16 00:31:48 +0900

    エラーページを追加

--- src/components/layout/PageTitle.tsx
+++ src/components/layout/PageTitle.tsx
@@ -1,12 +1,17 @@
+import classNames from "classnames";
 import { FC, ReactNode } from "react";
 import Center from "../Center";
 
 interface PageTitleProps {
     children?: ReactNode
+    className?: string
 }
-const PageTitle: FC<PageTitleProps> = ({ children }) => {
+const PageTitle: FC<PageTitleProps> = ({ children, className, }) => {
     return (
-        <h1 className="text-4xl md:text-6xl font-dot mt-16 md:mt-32 mb-14 sm font-bold text-center">
+        <h1 className={classNames(
+            "text-4xl md:text-6xl font-dot mt-16 md:mt-32 mb-14 sm font-bold text-center",
+            className,
+        )}>
             <Center>
                 {children}
             </Center>
--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,6 +1,7 @@
 import Center from '@/components/Center';
 import BasicLayout from '@/components/layout/BasicLayout';
 import LayoutContent from '@/components/layout/LayoutContent';
+import PageTitle from '@/components/layout/PageTitle';
 import { NextPage } from 'next';
 import Link from 'next/link';
 import { useRouter } from 'next/router';
@@ -10,17 +11,17 @@ interface Props {
 const NotFound: NextPage<Props> = ({ }) => {
     const router = useRouter()
     return (
-        <BasicLayout>
+        <BasicLayout className='bg-error text-error-content'>
+            <PageTitle>
+                Error: 404 Not Found !
+            </PageTitle>
             <LayoutContent>
-                <Center className='min-h-[70vh]'>
-                    <div className='text-5xl font-dot font-bold mt-2 mb-4'>
-                        404 Not Found !
-                    </div>
+                <Center>
                     <div className='my-8'>
                         指定されたURLは見つからなかったか削除されたページです。
                         URLが正しいか確認してください。
                     </div>
-                    <Link href="/" className='btn btn-link text-xl'>
+                    <Link href="/" className='link link-primary text-xl my-2 mx-4'>
                         TOPへ
                     </Link>
                 </Center>
