74f7affb4f5ac19f2862968d7a3fb5af60d03b3e
TBSten <81161390+TBSten@users.noreply.github.com>
2023-05-22 22:09:20 +0900

    APIドキュメントにwork関連の内容を追加

--- src/server/about-api/work.ts
+++ src/server/about-api/work.ts
@@ -4,35 +4,60 @@ import dedent from "dedent-js"
 const route = "/api/work"
 
 export const work = dedent`
-# GET [${md.c(route)}]()
+# GET [${md.c(route)}](${route})
 
-:::message
-作成中
-:::
-
-
-TODO:概要を記載
+[作ったもの](/works)で公開しているものを取得するAPIです。
 
 ### リクエスト形式
 
-TODO:リクエスト形式を記載
+なし
 
 ### レスポンス形式
 
-TODO:レスポンス形式を記載
+- 作ったものオブジェクトの配列
+- 作ったものオブジェクトとは以下のような形式のオブジェクトです。
+
+${md.cb("json", `
+{
+    "title": タイトル,
+    "detail": 作ったものの説明,
+    "link": 作ったもののリンク,
+    "image": イメージ画像
+}
+`)}
 
 :::details リクエスト例
-${md.cb("shell:curlでのリクエスト例", `
-TODO:curlでのリクエスト例を記載
+${md.codeBlock("shell:curlでのリクエスト例1", `
+curl https://tbsten.me${route}
 `)}
-${md.cb("js:JavaScriptでのリクエスト例", `
-TODO:JSでのリクエスト例を記載
+${md.codeBlock("js:JavaScriptでのリクエスト例", `
+const monolog = await fetch("https://tbsten.me${route}")
+    .then(r=>r.json())
 `)}
 :::
 
 :::details レスポンス例
 ${md.cb("json", `
-// TODO:レスポンス例
+[
+    {
+        "title": "ポートフォリオサイト",
+        "detail": "謎にレトロゲーム風なポートフォリオサイトです。",
+        "link": "https://tbsten.me",
+        "image": "https://storage.googleapis.com/tbsten-me-public-content/portfolio.png"
+    },
+    {
+        "title": "Flowchart Build Executor",
+        "detail": "フローチャートを組み立てて実行するツールです！",
+        "link": "https://fbe.vercel.app",
+        "image": "https://storage.googleapis.com/tbsten-me-public-content/fbe.png"
+    },
+    {
+        "title": "guess RGB",
+        "detail": "表示された色のRGB値を当てるWebアプリです。",
+        "link": "https://guess-rgb.vercel.app/",
+        "image": "https://storage.googleapis.com/tbsten-me-public-content/guessRGB.png"
+    }
+]
 `)}
 :::
 
